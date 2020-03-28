bec4f08510dcd90b9aee4ce24c261283a85344f1
Andres Suarez <andres.dario1906@gmail.com>
2020-03-28 08:24:22 -0500

    [PANEL]: FIXING PROBLEM WITH PANEL

--- dashboard.js
+++ dashboard.js
@@ -20,27 +20,6 @@ var charging = '<div id="charging" class="col-sm-12">'+
  		'</div>'+
 	'</div>';
 
-var panel = '<div style="height: 100vh; border-left: 10px solid red">'+
-		'<div class="container">'+
-		'<img style="width:80%; margin: auto; display: block; padding-top: 120px;" src="https://www.intersoftware.org.co/sites/default/files/logos_marcas_internas/logos-21.png">'+
-		'<h3 style="font:normal bold 23px Segoe UI; text-align: center">Seleccione su interés:</h3>'+
-		'<form id="formGroupPrincipal">'+
-		'<div class="form-group">'+
-		'<select class="form-control" id="sel1" style="border-radius: 8px!important;" name="nieve">'+
-		'<option>Inicio</option>'+
-		'<option>Tiempo por Ciudad</option>'+
-		'<option>Tiempo por Institución</option>'+
-		'<option>Mejores Instituciones</option>'+
-		'<option>Protocolos por Investigador</option>'+
-		'<option>Protocolos suspendidos</option>'+
-		'</select>'+
-		'</div>'+
-		'</form>'+
-		'<h6 id="copyright" style="width:80%; margin: auto; text-align: center; position: relative; top: 100px; font-weight:100; font: 12px black Times New Roman, sans-serif;">'+
-		'ESTE PROGRAMA ES DESARROLLADO COMO DEMOSTRACIÓN DE UN PRODUCTO Y NO PUEDE SER COPIADO NI EDITADO O DISTRIBUIDO SIN LA AUTORIZACIÓN EXPLICITA DE SU CREADOR'+
-		'</h6>'+
-		'</div>';
-
 var dashboard0 = '<div style="padding:15%; height: 100vh;" id="dash1">'+
 				'</div>'+
 				'<div class="centerTextPanel">'+
